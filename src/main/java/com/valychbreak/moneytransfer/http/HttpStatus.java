77a34202fd78a6b7e95e79a3036d71ddf211eff2
valychbreak <valychbreak@gmail.com>
2019-10-09 19:40:08 +0300

    Fixed bad request wrong status code

--- src/main/java/com/valychbreak/moneytransfer/http/HttpStatus.java
+++ src/main/java/com/valychbreak/moneytransfer/http/HttpStatus.java
@@ -2,5 +2,5 @@ package com.valychbreak.moneytransfer.http;
 
 public class HttpStatus {
     public static final int OK = 200;
-    public static final int BAD_REQUEST = 200;
+    public static final int BAD_REQUEST = 400;
 }
